499123fc45632444017209931354d230a8a433e3
Yazid Istiqlal Adhy Fadhillah <110552013+Y4zidd@users.noreply.github.com>
2025-05-10 11:52:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ const CONFIG = {
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
-	trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
+	// trackLocation: false, // If false or an error occurs, the app will use the lat/lon below
 	defaultLatitude: '-6.914744',
 	defaultLongitude: '107.609810',
 
